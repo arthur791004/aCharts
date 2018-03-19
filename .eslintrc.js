b5ca4e0908121b2c7db504ff6ff9e32a6ea5f52b
arthur791004 <arthur791004@gmail.com>
2018-03-19 09:45:42 +0800

    add eslint and stylelint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": "@17media/eslint-config-17media/browser",
+  "globals": {
+  },
+  "settings": {
+    "import/resolver": {
+      "babel-module": {}
+    }
+  },
+  "rules": {
+  },
+};
